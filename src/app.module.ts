ed723aa3a118e2fc40ffcb6cd25a2c800c453ce9
Ruben <rubenponce@live.com>
2025-03-11 21:36:08 -0300

    creating notifications gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
+import { NotificationsGateway } from './websocket/notifications/notifications.gateway';
 
 @Module({
   imports: [UsersModule, ProductsModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, NotificationsGateway],
 })
 export class AppModule {}
